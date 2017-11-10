30755edd19fa29327524f0de11364f428bcf6fb8
Liz Burton <liz.burton147@gmail.com>
2017-11-10 18:23:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,10 +12,10 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if !(position_taken?(board, index)) && index.between?(0,8)
-    true
-  else
+  if position_taken?(board, index) || !(index.between?(0,8))
     false
+  else
+    true
   end
 end
 
